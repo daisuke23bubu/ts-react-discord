1d6896f03edafc1bb1ae8d57b5a25e81572245aa
katayaman <daisuke23bubu@gmail.com>
2023-03-17 19:54:02 +0900

    feat: sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Home */}
     </div>
   );
 }
